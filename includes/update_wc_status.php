6292bd5549562eb48b7a54f1c7755eaef41da33d
maximverse <98321566+maximverse@users.noreply.github.com>
2022-07-21 14:00:58 +0300

    Revert "Fixed issue. Implemented functionaltiy for modal."
    This reverts commit 9feb7af037b1fb39f0501644ee9e2ee9e500c3d8.

--- includes/update_wc_status.php
+++ includes/update_wc_status.php
@@ -37,6 +37,9 @@ function update_wc_status_ajax()
     $metadata = (object) $paymentDetails->metadata;
     $status = $paymentDetails->status;
 
+    print_r('$status '.$status);
+
+
     if (!isset($metadata->{'order-id'})) {
         $data = array(
             'error' => 'Order not found',
